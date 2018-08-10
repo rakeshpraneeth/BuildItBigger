719d349de8282036d565153628db4da1846aac66
Rakesh Konduru <rakeshpraneeth@gmail.com>
2018-08-10 17:06:00 -0400

    * Created a 'jokesandroidlib' library to handle display of jokes.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,12 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
+import com.krp.jokesandroidlib.JokeDisplayActivity;
 import com.krp.jokesprovider.JokesGenerator;
 
 
@@ -46,9 +47,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
 
-
-        Toast.makeText(this, jokesGenerator.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokeDisplayActivity.class);
+        intent.putExtra(JokeDisplayActivity.EXTRA_JOKE_KEY,jokesGenerator.getJoke());
+        startActivity(intent);
     }
-
 
 }
---
+++ jokesandroidlib/src/main/java/com/krp/jokesandroidlib/JokeDisplayActivity.java
@@ -0,0 +1,45 @@
+package com.krp.jokesandroidlib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.MenuItem;
+
+public class JokeDisplayActivity extends AppCompatActivity {
+
+    AppCompatTextView jokeTv;
+    public static final String EXTRA_JOKE_KEY = "extraJokeKey";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke_display);
+        setTitle(R.string.joke);
+
+        jokeTv = findViewById(R.id.jokeTv);
+
+        if (getIntent() != null) {
+            String joke = getIntent().getStringExtra(EXTRA_JOKE_KEY);
+            if (joke != null) {
+                jokeTv.setText(joke);
+            } else {
+                jokeTv.setText(getString(R.string.joke_failure));
+            }
+        }
+
+        if(getSupportActionBar() !=null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }else{
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
