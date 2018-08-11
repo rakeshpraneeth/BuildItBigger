d10bb522636521b01957b565a3ccc7e5eda58bb1
Rakesh Konduru <rakeshpraneeth@gmail.com>
2018-08-10 21:10:38 -0400

    * Test refined.

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/RetrieveJokeAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/RetrieveJokeAsyncTaskTest.java
@@ -9,6 +9,8 @@ import java.util.concurrent.CountDownLatch;
 
 import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
@@ -27,6 +29,7 @@ public class RetrieveJokeAsyncTaskTest {
             public void onJokeReceived(String output) {
                 assertNotNull(output);
                 assertThat(output,is(any(String.class)));
+                assertThat( output, is(not(isEmptyOrNullString())));
                 countDownLatch.countDown();
             }
         }).execute();
